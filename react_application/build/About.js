234d0f426606a683767516713805d02529f093dd
Jeffrey Bue <jeffreyjbue@gmail.com>
2020-08-21 14:04:28 -0700

    Google Analytics.

--- react_application/build/About.js
+++ react_application/build/About.js
@@ -6,7 +6,7 @@ const About = () => <section id="about">
 	<div className="wrapper">
 		<h2>About Me</h2>
 	    <p>
-	        As a Sr. Application and Front-End Web Developer, I specialize in HTML, CSS, Javascript, Jquery, PHP, and MYSQL. I’m interested in ever increasing my knowledge with Javascript(including jQuery, Ajax, and node.js), the WordPress platform, and PCI Compliance Ecommerce. Always a team player I have an amazing niche for solving problems and getting the job done. I’m also extremely reliable to multitask on various projects at one time, ranging from graphic design to hard code and I guarantee it will be completed in a timely and efficient manner.
+	        As a Full Stack Developer, I specialize in HTML, CSS, Javascript, Jquery, Node.js and React, and PHP and MYSQL. I’m interested in ever increasing my knowledge with Javascript(including jQuery, Ajax, React, and node.js), the WordPress platform, and PCI Compliance Ecommerce. Always a team player I have an amazing niche for solving problems and getting the job done. I’m also extremely reliable to multitask on various projects at one time, ranging from graphic design to hard code and I guarantee it will be completed in a timely and efficient manner.
 	    </p>
 	    <p>
 	        I also love photography and videography.
