c5024b5fc4d6bc2e0ce20fdda0ce1afc246d27d3
Jeffrey Bue <jeffreyjbue@gmail.com>
2020-08-25 12:33:05 -0700

    Spiritual dirt without www.

--- react_application/portfolio/spiritualdirt/index.js
+++ react_application/portfolio/spiritualdirt/index.js
@@ -15,7 +15,7 @@ export const config = {
 			Spiritualdirt.com is my first ever Node.js / Express app. It was an idea I had when I was thinking about winning the lottery, I was looking at the beach and wondered how many grains a sands there were, and if I were to sell enough grains a sand, each for a fraction of a cent, I wouldn’t have to win the lottery. Also it was a great excuse to get experience on node.js
 		</p>
 		<p className="button">
-			<a className="btn" href="http://www.spiritualdirt.com" title="Spiritual Dirt" target="_blank">
+			<a className="btn" href="http://spiritualdirt.com" title="Spiritual Dirt" target="_blank">
 				Go To Spiritual Dirt
 			</a>
 		</p>
